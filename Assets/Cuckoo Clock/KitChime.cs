74d2ef4cdc14b58dc7ce3152dbb0a7cf754d0b1d
bamforjo <bamforjo@OA-S141-12>
2025-03-20 11:23:58 -0400

    Began working on making the cuckoo show when the chime happens. Need to add reference to transform. Left the project with some errors so changes commented out.

--- Assets/Cuckoo Clock/KitChime.cs
+++ Assets/Cuckoo Clock/KitChime.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class KitChime : MonoBehaviour
 {
+    public Transform theBird;
+    public float timeBirdIsOut = 2;
+    public float t;
+    
     public void Chime(int hour)
     {
         Debug.Log("Chiming" + hour + "o'clock !");
+       // StartCoroutine(Cuckoo());
     }
     public void ChimeWithoutArguments()
     {
         Debug.Log("Chiming !");
     }
+
+    //private IEnumerator Cuckoo()
+   // {
+    //    t = 0;
+     //   while(t < timeBirdIsOut)
+    //    {
+     //       t += Time.deltaTime;
+           // theBird.localPosition(0, 3, 0);
+       // }
+        
+   // }
 }
