cb2165f7c44576d43e4f27b13633c3120bb79b40
bamforjo <bamforjo@OA-S141-12>
2025-04-03 11:44:33 -0400

    Added Event Trigger for button A to test the highlight colour when hovering over it. Detection works but color is not.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    public Image buttonImage;
+    Color highlightColor = new Color(255f, 218f, 69f);
+    Color regularColor = new Color(185f, 185f, 185f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MouseHoveringOverButton()
+    {
+        buttonImage.color = highlightColor;
+        Debug.Log("Mouse is over button");
+    }
+
+    public void MouseLeavingButton()
+    {
+        buttonImage.color = regularColor;
+        Debug.Log("Mouse is no longer over button");
+    }
+}
