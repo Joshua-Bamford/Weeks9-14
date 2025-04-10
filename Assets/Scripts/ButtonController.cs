6f5df4575b03e5831cc2ffd38ded25551f01f482
Joshua Bamford <bamforjo@shernet.sheridancollege.ca>
2025-04-10 14:52:49 -0400

    Got the buttons working by creating a controller object with the script in it. But now there is nothing using UnityEvents.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -2,50 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
-   // public SpriteRenderer buttonImage;
-    Color highlightColor = new Color(255f, 218f, 69f);
-    Color regularColor = new Color(178f, 178f, 785f);
+ 
     public int[] inputSequence = {0, 0, 0, 0};
-    public UnityEvent HoveringOver;
-    public UnityEvent HoveringOut;
     public UnityEvent ButtonClicked;
-    bool previouslyHovering = false;
-    SpriteRenderer buttonSprite;
     public int valueOfButtonPressed;
-    
-
+    //public GameObject buttonA;
+   // public GameObject buttonB;
+   // public GameObject buttonC;
+   // public GameObject buttonD;
+    public SpriteRenderer buttonASprite;
+    public SpriteRenderer buttonBSprite;
+    public SpriteRenderer buttonCSprite;
+    public SpriteRenderer buttonDSprite;
     // Start is called before the first frame update
     void Start()
     {
-        buttonSprite = GetComponent<SpriteRenderer>();
-     inputSequence = new int[4];
+       // buttonASprite = GetComponent<SpriteRenderer>();
+       // buttonBSprite = GetComponent<SpriteRenderer>();
+       // buttonCSprite = GetComponent<SpriteRenderer>();
+       // buttonDSprite = GetComponent<SpriteRenderer>();
+        inputSequence = new int[4];
     }
 
     // Update is called once per frame
     void Update()
     {
        Debug.Log("Current Answer" + inputSequence[0] + inputSequence[1] + inputSequence[2] + inputSequence[3]);
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (buttonSprite.bounds.Contains(mousePosition))
-        {
-            previouslyHovering = true;
-            HoveringOver.Invoke();
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);    //record current position of mouse
 
+        if (buttonASprite.bounds.Contains(mousePosition))   
+        {
             if (Input.GetMouseButtonDown(0)) {
-                ButtonClicked.Invoke();
+                AddInputSequence(1);
+               // ButtonClicked.Invoke();
             }
         }
-        if (!buttonSprite.bounds.Contains(mousePosition) && previouslyHovering) 
+
+        if(buttonBSprite.bounds.Contains(mousePosition))
         {
-            HoveringOut.Invoke();
+            if (Input.GetMouseButtonDown(0))
+            {
+                AddInputSequence(2);
+              //  ButtonClicked.Invoke();
+            }
+        }
+
+        if (buttonCSprite.bounds.Contains(mousePosition))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                AddInputSequence(3);
+               // ButtonClicked.Invoke();
+            }
+        }
+
+        if(buttonDSprite.bounds.Contains(mousePosition))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                AddInputSequence(4);
+               // ButtonClicked.Invoke();
+            }
         }
     }
 
-    public void AddInputSequence()
+    public void AddInputSequence(int valueOfButtonPressed)
     {
         if (inputSequence[0] == 0)
         {
@@ -63,19 +87,15 @@ public class ButtonController : MonoBehaviour
         {
             inputSequence[3] = valueOfButtonPressed;
         }
-       
+        else
+        {
+            inputSequence[0] = 0;
+            inputSequence[1] = 0;
+            inputSequence[2] = 0;
+            inputSequence[3] = 0;
+        }
+
+
     }
 
-    public void HighlightButton()
-    {
-        buttonSprite.color = highlightColor;
-       // Debug.Log("Mouse is over button");
-    }
-    public void DeHighlightButton()
-    {
-            buttonSprite.color = regularColor;
-            previouslyHovering = false;
-        
-       // Debug.Log("Mouse is no longer over button");
-    }
 }
